33b4703f572c5575495e2ed339796c058b03bc29
dattaz <taz@dattaz.fr>
2017-05-08 20:52:43 +0200

    check if zimwriterfs is present only if --nozim is not present

--- youtube/youtube2zim.py
+++ youtube/youtube2zim.py
@@ -520,7 +520,7 @@ def get_playlist(url):
 
 def run():
     arguments = docopt(__doc__, version='youtube2zim 1.2.1')
-    if not bin_is_present("zimwriterfs"):
+    if not arguments['--nozim'] and not bin_is_present("zimwriterfs"):
         sys.exit("zimwriterfs is not available, please install it.")
 
     if arguments["--lowquality"]:
