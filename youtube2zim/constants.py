39c3bc13cf83be834c85cffd240e8b18f4ed9ac3
renaud gaudin <rgaudin@gmail.com>
2019-12-31 12:09:27 +0000

    separating logs into stdout and stderr

--- youtube2zim/constants.py
+++ youtube2zim/constants.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # vim: ai ts=4 sts=4 et sw=4 nu
 
+import sys
 import pathlib
 import logging
 
@@ -14,8 +15,31 @@ PLAYLIST = "playlist"
 USER = "user"
 ROOT_DIR = pathlib.Path(__file__).parent
 
-logging.basicConfig(format="%(levelname)s:%(message)s")
+
+class LessThanFilter(logging.Filter):
+    def __init__(self, exclusive_maximum, name=""):
+        super(LessThanFilter, self).__init__(name)
+        self.max_level = exclusive_maximum
+
+    def filter(self, record):
+        """ non-zero return means we log this message """
+        return 1 if record.levelno < self.max_level else 0
+
+
 logger = logging.getLogger("youtube-scraper")
+logger.setLevel(logging.NOTSET)
+log_format = "[%(asctime)s] %(levelname)s:%(message)s"
+
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setFormatter(logging.Formatter(log_format))
+stdout_handler.addFilter(LessThanFilter(logging.WARNING))
+logger.addHandler(stdout_handler)
+stdout_handler.setLevel(logging.DEBUG)
+
+stderr_handler = logging.StreamHandler(sys.stderr)
+stdout_handler.setFormatter(logging.Formatter(log_format))
+stderr_handler.setLevel(logging.WARNING)
+logger.addHandler(stderr_handler)
 
 
 class Youtube(object):
