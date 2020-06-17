d694ff15699f6d642a4dd03e64c354b7242c6a89
renaud gaudin <rgaudin@gmail.com>
2020-06-17 16:43:59 +0000

    Fixed #103: account for webp thumbnails
    On newest youtube-dl, the thumbnails are downloaded as webp.
    This makes sure we exclude video.webp when looking for video file (extension unknown)
    and that we convert it to jpg before continuing.

--- youtube2zim/processing.py
+++ youtube2zim/processing.py
@@ -3,9 +3,15 @@
 # vim: ai ts=4 sts=4 et sw=4 nu
 
 from zimscraperlib.video.encoding import reencode
-from zimscraperlib.imaging import resize_image
+from zimscraperlib.imaging import resize_image, PIL
 
 from .constants import logger
+
+
+def convert_thumbnail(source_image, dest_image):
+    """ convert image using destination's filename as destination format """
+    img = PIL.Image.open(source_image)
+    img.save(str(dest_image))
 
 
 def post_process_video(
@@ -17,7 +23,12 @@ def post_process_video(
         - recompress video if incorrect video_format or low_quality requested """
 
     # find downloaded video from video_dir
-    files = [p for p in video_dir.iterdir() if p.stem == "video" and p.suffix != ".jpg"]
+    files = [
+        p
+        for p in video_dir.iterdir()
+        if p.stem == "video" and p.suffix not in (".jpg", ".webp")
+    ]
+
     if len(files) == 0:
         logger.error(f"Video file missing in {video_dir} for {video_id}")
         logger.debug(list(video_dir.iterdir()))
@@ -28,17 +39,21 @@ def post_process_video(
         )
     src_path = files[0]
 
+    # do we need to convert thumbnail?
+    thumb_src = src_path.with_name("video.jpg")
+    if not thumb_src.exists():
+        logger.debug("We don't have video.jpg, let's convert from webp")
+        convert_thumbnail(thumb_src.with_suffix(".webp"), thumb_src)
+
     # resize thumbnail. we use max width:248x187px in listing
     # but our posters are 480x270px
-    resize_image(
-        src_path.parent.joinpath("video.jpg"), width=480, height=270, method="cover"
-    )
+    resize_image(thumb_src, width=480, height=270, method="cover")
 
     # don't reencode if not requesting low-quality and received wanted format
     if skip_recompress or (not low_quality and src_path.suffix[1:] == video_format):
         return
 
-    dst_path = src_path.parent.joinpath(f"video.{video_format}")
+    dst_path = src_path.with_name(f"video.{video_format}")
     reencode(
         src_path, dst_path, preset.to_ffmpeg_args(), delete_src=True, failsafe=False
     )
