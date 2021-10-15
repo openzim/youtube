d749e318ac294839a0605b540b7947d8a9deba71
renaud gaudin <rgaudin@gmail.com>
2021-10-15 10:13:43 +0000

    Fixed #150: setting controls=true on HTML source
    So that JS-disabled browser can still properly control video playback

--- youtube2zim/templates/assets/app.js
+++ youtube2zim/templates/assets/app.js
@@ -139,6 +139,7 @@ function firstVideo(video) {
     videoIntro.innerHTML = '' +
         '<video id="video_container" class="video-js vjs-default-skin" ' +
                'width="480px" height="270px" crossorigin ' +
+               'controls="true" preload="true" ' +
                'data-setup=\'{"techOrder": ["html5", "ogvjs"], ' +
                             '"ogvjs": {"base": "assets/ogvjs"}, "autoplay": false, ' +
                                       '"preload": true, "controls": true, "controlBar": {"pictureInPictureToggle": false}}\'' +
