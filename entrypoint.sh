3ef75e5ac5e3c1849ff89c731d35cdda6ce803d2
renaud gaudin <reg@rskg.org>
2022-01-21 14:32:39 +0000

    TEMP: using a commit-based youtube-dl release to workaround a new YT issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 echo "Attempting to update youtube-dl…"
-pip3 install -U youtube-dl
+# pip3 install -U youtube-dl
+# TEMP: awaiting post 2021.12.17 which would include an important fix
+pip3 install --force-reinstall -U git+https://github.com/ytdl-org/youtube-dl@905d1d281ddfa5d183fc445010d350cefc6a58ec
+
 
 exec "$@"
