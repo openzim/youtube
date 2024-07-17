e1b07052b243662ec7157e7ccd188b6b2f5443a5
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2024-07-17 20:15:27 +0530

    Update Vite configuration in zimui for native ES6 support

--- zimui/vite.config.ts
+++ zimui/vite.config.ts
@@ -3,14 +3,27 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
+import legacy from '@vitejs/plugin-legacy'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
+  esbuild: {
+    target: 'es2015',
+    include: /\.(ts|jsx|tsx)$/
+  },
+  build: {
+    target: 'es2015'
+  },
   plugins: [
     vue(),
     vuetify({ autoImport: true }),
+    legacy({
+      targets: ['fully supports es6'],
+      renderLegacyChunks: false,
+      modernPolyfills: true
+    }),
     viteStaticCopy({
       targets: [
         {
